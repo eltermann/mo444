5eba41eefa42113a6db79e946fa0efead6afc6cc
Felipe Eltermann <felipe@wearepropeople.com>
2013-10-09 17:06:19 -0300

    Adding aux methods on persistency layer

--- data-retrieval/PersistencyLayer.py
+++ data-retrieval/PersistencyLayer.py
@@ -12,8 +12,68 @@ class TweetsPersister():
                                     user = credentials['raw-tweets']['user'],
                                     passwd = credentials['raw-tweets']['passwd'])
 
+      self.db = MySQLdb.connect(host = credentials['structured-tweets']['host'],
+                                db = credentials['structured-tweets']['db'],
+                                user = credentials['structured-tweets']['user'],
+                                passwd = credentials['structured-tweets']['passwd'])
+
 
    def insertRawTweet(self, string):
       c = self.db_raw.cursor()
       c.execute("INSERT INTO raw_tweets (tweet) VALUES (%s)", string)
       self.db_raw.commit()
+
+
+   def loadTweet(self, tweet_id):
+      """
+      Tweet loader.
+      @param tweet_id
+      @return Populated tweet dictionary, if tweet found. "None" otherwise.
+      """
+      c = self.db.cursor()
+      c.execute("SELECT tweet_text, tweet_source, user_id, tweet_retweeted_status_id FROM tweet WHERE tweet_id = %s", tweet_id)
+      row = c.fetchone()
+      if (row is None):
+         return None
+
+      tweet = {
+         'id': tweed_id,
+         'text': row[0],
+         'source': row[1],
+         'user_id': row[2],
+         'retweeted_status_id': row[3],
+      }
+
+      return tweet
+
+
+   def loadUser(self, user_id):
+      """
+      User loader.
+      @param user_id
+      @return Populated user dictionary, if found. "None" otherwise.
+      """
+      c = self.db.cursor()
+      c.execute("SELECT user_name, user_screen_name, user_location, user_description, user_url, user_followers_count, user_friends_count FROM user WHERE user_id = %s", user_id)
+      row = c.fetchone()
+      if (row is None):
+         return None
+
+      user = {
+         'id': user_id,
+         'name': row[0],
+         'screen_name': row[1],
+         'location': row[2],
+         'description': row[3],
+         'url': row[4],
+         'followers_count': row[5],
+         'friends_count': row[6],
+      }
+
+      return user
+
+   def saveTweet(self, tweet):
+      # TODO
+
+   def saveUser(self, user):
+      # TODO
