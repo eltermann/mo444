de8ab30c442eed6557db7f56959f7820837439b6
eltermann <felipe.eltermann@gmail.com>
2013-11-15 08:01:02 -0200

    Enhanced PopulateRetweetsHistory.py

--- data-retrieval/PopulateRetweetsHistory.py
+++ data-retrieval/PopulateRetweetsHistory.py
@@ -16,7 +16,6 @@ params = (
    #','.join(followedUserIds)
 )
 
-#c2.execute("SELECT tweet_id, tweet_created_at FROM tweet_with_created_at2 WHERE tweet_created_at > %s AND tweet_retweeted_status_id = 0 AND user_id IN (%s)", params)
 c2.execute("SELECT tweet_id, tweet_created_at FROM tweet_with_created_at2 WHERE tweet_created_at > %s AND tweet_retweeted_status_id = 0", params)
 
 original_rows = c2.fetchall()
@@ -25,21 +24,35 @@ for original_row in original_rows:
    original_created_at = original_row[1]
    print "original_tweet: %s" % (original_tweet_id)
 
-   c1.execute("SELECT tweet_id, tweet_created_at FROM tweet_with_created_at2 WHERE tweet_retweeted_status_id = %s", (original_tweet_id))
+   c1.execute("SELECT tweet_id, tweet_created_at, user_id FROM tweet_with_created_at2 WHERE tweet_retweeted_status_id = %s", (original_tweet_id))
 
    rows = c1.fetchall()
-   count = 1
+   count = 0
+   reached_followers_count = 0
+   reached_friends_count = 0
+   reached_favorited_count = 0
    for row in rows:
       retweet_id = row[0]
       retweet_created_at = row[1]
+      user_id = row[2]
+
+      user = persister.loadUser(user_id)
+
+      count += 1
+      reached_followers_count += user["followers_count"]
+      reached_friends_count += user["friends_count"]
+      reached_favorited_count += user["favorited_count"]
       data = (
          original_tweet_id,
          retweet_id,
          int(retweet_created_at) - int(original_created_at),
-         count
+         count,
+         reached_followers_count,
+         reached_friends_count,
+         reached_favorited_count
       )
-      c1.execute("INSERT INTO retweets_history2 (original_tweet_id, retweet_id, elapsed_time, count) VALUES (%s, %s, %s, %s)", data)
+      c1.execute("INSERT INTO retweets_history4 (original_tweet_id, retweet_id, elapsed_time, count, reached_followers_count, reached_friends_count, reached_favorited_count) VALUES (%s, %s, %s, %s, %s, %s, %s)", data)
       persister.db.commit()
-      count += 1
+
 
    original_row = c2.fetchone()
