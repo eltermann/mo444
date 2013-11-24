d9505fa1f39099ff03bd9edad572565fafef2c5c
eltermann <felipe.eltermann@gmail.com>
2013-11-24 12:55:03 -0200

    Added test code

--- analysis/analyse.R
+++ analysis/analyse.R
@@ -1,4 +1,4 @@
-mo444LinearRegression <- function(use.grouped=FALSE, min.retweets.count=1, time.stamps.from=1, time.stamps.to=28) {
+mo444LinearRegression <- function(use.grouped=FALSE, min.retweets.count=1, max.retweets.count=1000000, time.stamps.from=1, time.stamps.to=28) {
   library('MASS')
 
   if (use.grouped) {
@@ -9,7 +9,7 @@ mo444LinearRegression <- function(use.grouped=FALSE, min.retweets.count=1, time.
     local.feature.vectors <- feature.vectors.norm
     local.observation.data <- observation.data
   }
-  indexes <- which(local.observation.data > min.retweets.count)
+  indexes <- which(local.observation.data >= min.retweets.count & local.observation.data <= max.retweets.count)
   local.feature.vectors <- local.feature.vectors[indexes,c(time.stamps.from:time.stamps.to, (28+time.stamps.from):(28+time.stamps.to), 57:70)]
   local.observation.data <- local.observation.data[indexes]
 
@@ -50,6 +50,7 @@ mo444LinearRegression <- function(use.grouped=FALSE, min.retweets.count=1, time.
   ret[['J.validation']] <- J.validation
   ret[['hit.rate.train']] <- hit.rate.train
   ret[['hit.rate.validation']] <- hit.rate.validation
+  ret[['local.num.tweets']] <- local.num.tweets
 
   return(ret)
 }
@@ -117,7 +118,7 @@ mo444PolynomialRegression <- function(use.grouped=FALSE, min.retweets.count=1, t
   return(ret)
 }
 
-mo444EuclidianDistanceToKMeans <- function(use.grouped=FALSE, time.stamps=28) {
+mo444EuclidianDistanceToKMeans <- function(use.grouped=FALSE, min.retweets.count=1, max.retweets.count=1000000, time.stamps=28, num.clusters=100) {
   if (use.grouped) {
     local.feature.vectors <- feature.vectors.norm.grouped
     local.observation.data <- observation.data.grouped
@@ -126,7 +127,7 @@ mo444EuclidianDistanceToKMeans <- function(use.grouped=FALSE, time.stamps=28) {
     local.feature.vectors <- feature.vectors.norm
     local.observation.data <- observation.data
   }
-  indexes <- which(local.observation.data > min.retweets.count)
+  indexes <- which(local.observation.data >= min.retweets.count & local.observation.data <= max.retweets.count)
 
   local.feature.vectors <- local.feature.vectors[indexes,c(1:time.stamps, 29:(28+time.stamps), 57:70)]
   local.observation.data <- local.observation.data[indexes]
@@ -139,7 +140,7 @@ mo444EuclidianDistanceToKMeans <- function(use.grouped=FALSE, time.stamps=28) {
   observation.data.train <- local.observation.data[1:local.train.size]
   observation.data.validation <- local.observation.data[(local.train.size+1):local.num.tweets]
 
-  num.clusters <- ifelse(use.grouped, 50, 500)
+  #num.clusters <- ifelse(use.grouped, 50, 500)
   km <- kmeans(feature.vectors.train, centers=num.clusters, iter.max=500)
   km.retweets.means <- sapply(1:num.clusters, function(x) {
     return(mean(observation.data.train[which(km$cluster == x)]))
@@ -154,6 +155,11 @@ mo444EuclidianDistanceToKMeans <- function(use.grouped=FALSE, time.stamps=28) {
 
   # calculate errors
   J.validation <- mean((estimated.results.validation - observation.data.validation)^2)
+  minmax.validation <- data.frame(min=0.85*observation.data.validation, max=1.15*observation.data.validation)
+  hit.rate.validation <- sapply(1:length(estimated.results.validation), function(x) {
+    return(ifelse(estimated.results.validation[x]>=minmax.validation[x, 'min'] && estimated.results.validation[x]<=minmax.validation[x, 'max'], 1, 0))
+  })
+  hit.rate.validation <- mean(hit.rate.validation)
 
   ret <- list()
   ret[['observation.data.train']] <- observation.data.train
@@ -162,11 +168,13 @@ mo444EuclidianDistanceToKMeans <- function(use.grouped=FALSE, time.stamps=28) {
   ret[['observation.data.validation']] <- observation.data.validation
   ret[['estimated.results.validation']] <- estimated.results.validation
   ret[['J.validation']] <- J.validation
+  ret[['hit.rate.validation']] <- hit.rate.validation
+  ret[['local.num.tweets']] <- local.num.tweets
   
   return(ret)
 }
 
-mo444DecisionTree <- function(use.grouped=FALSE, min.retweets.count=1, time.stamps=28) {
+mo444DecisionTree <- function(use.grouped=FALSE, min.retweets.count=1, max.retweets.count=1000000, time.stamps=28) {
   library('rpart')
 
   if (use.grouped) {
@@ -177,7 +185,7 @@ mo444DecisionTree <- function(use.grouped=FALSE, min.retweets.count=1, time.stam
     local.feature.vectors <- data.frame.norm
     local.observation.data <- observation.data
   }
-  indexes <- which(local.observation.data > min.retweets.count)
+  indexes <- which(local.observation.data >= min.retweets.count & local.observation.data <= max.retweets.count)
   local.feature.vectors <- local.feature.vectors[indexes,c(1:time.stamps, 29:(28+time.stamps), 57:71)]
   local.observation.data <- local.observation.data[indexes]
 
@@ -227,6 +235,7 @@ mo444DecisionTree <- function(use.grouped=FALSE, min.retweets.count=1, time.stam
   ret[['J.validation']] <- J.validation
   ret[['hit.rate.train']] <- hit.rate.train
   ret[['hit.rate.validation']] <- hit.rate.validation
+  ret[['local.num.tweets']] <- local.num.tweets
   
   return(ret)
 }
@@ -386,10 +395,10 @@ mo444NeuralNetworkELM <- function(use.grouped=FALSE, min.retweets.count=1, time.
   hidden.layer.test <- cbind(tanh(X.test%*%t(W1)), 1)
   Y.test.est = hidden.layer.test %*% W2
   MSE.test <- mean((Y.test.est - Y.test)^2)
-  
+
   minmax.validation <- data.frame(min=0.85*Y.test, max=1.15*Y.test)
-  hit.rate.validation <- sapply(1:length(estimated.results.validation), function(x) {
-    return(ifelse(estimated.results.validation[x]>=minmax.validation[x, 'min'] && estimated.results.validation[x]<=minmax.validation[x, 'max'], 1, 0))
+  hit.rate.validation <- sapply(1:length(Y.test.est), function(x) {
+    return(ifelse(Y.test.est[x]>=minmax.validation[x, 'min'] && Y.test.est[x]<=minmax.validation[x, 'max'], 1, 0))
   })
   hit.rate.validation <- mean(hit.rate.validation)
   
@@ -398,6 +407,144 @@ mo444NeuralNetworkELM <- function(use.grouped=FALSE, min.retweets.count=1, time.
   ret[['estimated.results.validation']] <- Y.test.est
   ret[['MSE.test']] <- MSE.test
   ret[['hit.rate.validation']] <- hit.rate.validation
+  return(ret)
+}
+
+mo444NeuralNetworkMLP <- function(use.grouped=FALSE, min.retweets.count=1, max.retweets.count=1000000, time.stamps=27, hidden.neurons=800, n.epochs=5000) {
+  if (use.grouped) {
+    local.feature.vectors <- feature.vectors.norm.grouped
+    local.observation.data <- observation.data.grouped
+  }
+  else {
+    local.feature.vectors <- feature.vectors.norm
+    local.observation.data <- observation.data
+  }
+  indexes <- which(local.observation.data >= min.retweets.count & local.observation.data <= max.retweets.count)
+  local.feature.vectors <- local.feature.vectors[indexes,c(1:time.stamps, 29:(28+time.stamps), 57:70)]
+  local.observation.data <- local.observation.data[indexes]
+
+  local.num.tweets <- length(local.observation.data)
+  local.train.size <- 0.8 * local.num.tweets
+
+  #separate training and test data
+  fv <- local.feature.vectors[1:local.train.size,]
+  X.test <- local.feature.vectors[(local.train.size+1):local.num.tweets,]
+  Y <- as.matrix(local.observation.data[1:local.train.size])
+  Y.test <- as.matrix(local.observation.data[(local.train.size+1):local.num.tweets])
+
+  #Neural network
+  #ML
+
+  #separate train and validation data
+  #fv <- cbind(fv, 1) #add bias
+  split1 <- round(0.8*nrow(fv))
+  X.train <- fv[1:split1, ]
+  Y.train <- as.matrix(Y[1:split1, ])
+  X.val <- fv[(split1+1):nrow(fv),]
+  Y.val <- as.matrix(Y[(split1+1):nrow(fv),])
+
+  #get the number of inputs and outputs
+  n.elem <- nrow(X.train)
+  n.inputs <- ncol(X.train)
+  n.outputs <- ncol(Y.train)
+
+  #Random Initialize weights matrices
+  W1 <- matrix(rnorm(hidden.neurons*n.inputs), nrow=hidden.neurons, ncol=n.inputs)
+  W2 <- matrix(rnorm(n.outputs*(hidden.neurons+1)), nrow=n.outputs, ncol=(hidden.neurons+1))
+  
+  #Define minimum error (stop condition)
+  threshold = 1e-5
+  MSE <- Inf
+  best.MSE.val = Inf
+
+  #save progression to analyse plot
+  MSE.train.prog <- c()
+  MSE.val.prog <- c()
+
+  #Define initial learning rate
+  alpha <- 1#/ncol(X.train)
+  current.epoch <- 1
+  while((current.epoch <= n.epochs)){# && ( MSE > threshold )){
+    #print(current.epoch)
+    #find the gradient
+    
+    #propagation
+    #forward propagation
+    hidden.layer <- tanh(X.train%*%t(W1))
+    Y.train.est <- cbind(hidden.layer, 1) %*% t(W2)
+    
+    #backpropagation
+    error <- Y.train.est - Y.train
+    dJ.dW2 <- t(error)%*%cbind(hidden.layer, 1)
+    
+    sig <- (error%*%W2[,1:hidden.neurons]) * ((1-hidden.layer)* hidden.layer)
+    dJ.dW1 <- t(sig) %*% X.train
+    
+    #normalize
+    gradient <- c(dJ.dW1, dJ.dW2)
+    dJ.dW2 <- dJ.dW2 / norm(matrix(gradient), "2")
+    dJ.dW1 <- dJ.dW1 / norm(matrix(gradient), "2")
+    
+    #update weights
+    W1.temp <- W1 - alpha*dJ.dW1
+    W2.temp <- W2 - alpha*dJ.dW2
+    
+    #calculate error
+    hidden.layer <- tanh(X.train%*%t(W1.temp))
+    Y.train.est <- cbind(hidden.layer, 1) %*% t(W2.temp)
+    
+    error <- Y.train.est - Y.train
+    MSE.temp <- mean(error^2)
+    
+    #update attributes
+    W1 <- W1.temp
+    W2 <- W2.temp
+    MSE <- MSE.temp
+
+    #get validation error
+    #calculate error
+    hidden.layer <- tanh(X.val%*%t(W1))
+    Y.val.est <- cbind(hidden.layer, 1) %*% t(W2)
+    
+    error <- Y.val.est - Y.val
+    MSE.val <- mean(error^2)
+    
+    if(MSE.val < best.MSE.val){
+      best.MSE.val <- MSE.val
+      best.W1 <- W1;
+      best.W2 <- W2;
+      best.epoch <- current.epoch
+    }
+    
+    #register progress
+    MSE.train.prog <- c(MSE.train.prog, MSE)
+    MSE.val.prog <- c(MSE.val.prog, MSE.val)
+    
+    current.epoch <- current.epoch + 1
+  }
+  
+  #plot(MSE.val.prog, type="l")
+  #plot(MSE.train.prog, type="l")
+
+  #do the prediction
+  hidden.layer <- tanh(X.test%*%t(best.W1))
+  Y.test.est <- cbind(hidden.layer, 1) %*% t(best.W2)
+
+  error <- Y.test.est - Y.test
+  MSE.test <- mean(error^2)
+  
+  minmax.validation <- data.frame(min=0.85*Y.test, max=1.15*Y.test)
+  hit.rate.validation <- sapply(1:length(Y.test.est), function(x) {
+    return(ifelse(Y.test.est[x]>=minmax.validation[x, 'min'] && Y.test.est[x]<=minmax.validation[x, 'max'], 1, 0))
+  })
+  hit.rate.validation <- mean(hit.rate.validation)
+  
+  ret <- list()
+  ret[['observation.data.validation']] <- Y.test
+  ret[['estimated.results.validation']] <- Y.test.est
+  ret[['MSE.test']] <- MSE.test
+  ret[['hit.rate.validation']] <- hit.rate.validation
+  ret[['local.num.tweets']] <- local.num.tweets
   return(ret)
 }
 
@@ -436,8 +583,28 @@ feature.vectors.norm.grouped <<- cbind(feature.vectors.norm.grouped, 1) # add bi
 data.frame.norm.grouped <<- data.frame(feature.vectors.norm.grouped, observation.data=observation.data.grouped)
 
 # Execute analysis
-regression.results <- mo444LinearRegression(use.grouped=F, min.retweets.count=1, time.stamps.from=20, time.stamps.to=26)
-decision.tree.results <- mo444DecisionTree(use.grouped=F, min.retweets.count=1, time.stamps=27)
-euclidian.distance.results <- mo444EuclidianDistanceToKMeans()
-svm.results <- mo444SVM(use.grouped=T)
-elm.results <- mo444NeuralNetworkELM(use.grouped=T)
+#regression.results <- mo444LinearRegression(use.grouped=F, min.retweets.count=1, max.retweets.count=1000000, time.stamps.from=1, time.stamps.to=27)
+#decision.tree.results <- mo444DecisionTree(use.grouped=F, min.retweets.count=1, time.stamps=27)
+#euclidian.distance.results <- mo444EuclidianDistanceToKMeans()
+#svm.results <- mo444SVM(use.grouped=T)
+#elm.results <- mo444NeuralNetworkELM(use.grouped=T)
+#mlp.results <- mo444NeuralNetworkMLP(use.grouped=T,hidden.neurons=100, n.epochs=100)
+
+# Kmeans test
+#for (clust in c(100, 200, 500)) {
+#  for (stamps in c(27, 21, 11)) {
+#    kmeans.results <- mo444EuclidianDistanceToKMeans(use.grouped=F, time.stamps=stamps, num.clusters=clust)
+#    print(paste("time.stamps=",stamps, ", num.clusters=", clust))
+#    print(data.frame(J.validation=kmeans.results$J.validation, hit.rate.validation=kmeans.results$hit.rate.validation))
+#  }
+#}
+
+# Decision tree
+#minmaxes <- list(c(1,2000000), c(1, 100), c(100, 2000), c(2000, 2000000))
+#for (minmax in minmaxes) {
+#  for (stamps in c(27, 21, 11)) {
+#    decision.tree.results <- mo444DecisionTree(min.retweets.count=minmax[1], max.retweets.count=minmax[2], time.stamps=stamps)
+#    print(paste("time.stamps=",stamps, ", min=", minmax[1], ", max=", minmax[2]))
+#    print(data.frame(J.validation=decision.tree.results$J.validation, hit.rate.validation=decision.tree.results$hit.rate.validation))
+#  }
+#}
