86bd4781be7bfdae104706386aef8fa752266a05
eltermann <felipe.eltermann@gmail.com>
2013-11-23 23:38:40 -0200

    Added mo444NeuralNetworkELM()

--- analysis/analyse.R
+++ analysis/analyse.R
@@ -268,6 +268,139 @@ mo444SVM <- function(use.grouped=FALSE) {
   return(ret)
 }
 
+mo444NeuralNetworkELM <- function(use.grouped=FALSE, min.retweets.count=1, time.stamps=27, n.hid=3000) {
+  library('MASS')
+
+  if (use.grouped) {
+    local.feature.vectors <- feature.vectors.norm.grouped
+    local.observation.data <- observation.data.grouped
+  }
+  else {
+    local.feature.vectors <- feature.vectors.norm
+    local.observation.data <- observation.data
+  }
+  indexes <- which(local.observation.data > min.retweets.count)
+  local.feature.vectors <- local.feature.vectors[indexes,c(1:time.stamps, 29:(28+time.stamps), 57:70)]
+  local.observation.data <- local.observation.data[indexes]
+
+  local.num.tweets <- length(local.observation.data)
+  local.train.size <- 0.8 * local.num.tweets
+
+  #separate training and test data
+  fv <- local.feature.vectors[1:local.train.size,]
+  fv.test <- local.feature.vectors[(local.train.size+1):local.num.tweets,]
+  Y <- as.matrix(local.observation.data[1:local.train.size])
+  Y.test <- as.matrix(local.observation.data[(local.train.size+1):local.num.tweets])
+  
+  #separate training and validation data
+  split <- round(0.8*nrow(fv))
+  X.train <- fv[1:split, ]
+  X.train <- as.matrix(cbind(X.train, 1))
+  Y.train <- as.matrix(Y[1:split, ])
+  X.val <- fv[(split+1):nrow(fv),]
+  X.val <- as.matrix(cbind(X.val, 1))
+  Y.val <- as.matrix(Y[(split+1):nrow(fv),])
+  
+  #get the number of inputs and outputs
+  n.elem <- nrow(X.train)
+  n.inputs <- ncol(X.train)
+  n.outputs <- ncol(Y.train)
+  
+  ###ELM
+  #1 - set weights of hidden layer randomly, in a normal distribution with mean=0 and std=1
+  W1 <- matrix(rnorm(n.hid*n.inputs), nrow=n.hid, ncol=n.inputs)
+  hidden.layer <- cbind(tanh(X.train%*%t(W1)), 1)
+  hidden.layer.val <- cbind(tanh(X.val%*%t(W1)), 1) #output from hidden layer, for validation data
+  
+  #2 - define weights of exit layer, as a linear combination of hidden neurons, minimizing it norm.
+  
+  #using golden search to define C
+  k <- (sqrt(5) - 1) / 2;
+  a <- 0.001
+  b <- 20 #a and c are the current bounds; the minimum is between them.
+  
+  xL <- b - k * (b - a);
+  xR <- a + k * (b - a);
+  
+  while(abs(b - a) > 0.0001){
+    #evaluate the function in xL and xR, where:
+    
+    #xL:
+    #solving system:
+    #I/C + Hw2 = Y.train
+    #w2 = pinv(I/C + H) Y.train
+    
+    if(n.hid <= n.elem){ #less neurons than samples
+      W2.xL <- ginv((diag(1, n.hid+1)/xL) + t(hidden.layer)%*%hidden.layer) %*% t(hidden.layer) %*% Y.train
+    }
+    else {
+      W2.xL <- t(hidden.layer) %*% ginv((diag(1, n.elem)/xL) + hidden.layer%*%t(hidden.layer)) %*% Y.train
+    }
+    
+    #use validation set to calculate error
+    Y.xL <- hidden.layer.val %*% W2.xL;
+    MSE.xL <- mean((Y.xL - Y.val)^2) #value of the function in point xL
+    
+    #the same for xR:
+    if(n.hid <= n.elem){ #less neurons than samples
+      W2.xR <- ginv((diag(1, n.hid+1)/xL) + t(hidden.layer)%*%hidden.layer) %*% t(hidden.layer) %*% Y.train
+    }
+    else {
+      W2.xR <- t(hidden.layer) %*% ginv((diag(1, n.elem)/xR) + hidden.layer%*%t(hidden.layer)) %*% Y.train
+    }
+    
+    Y.xR <- hidden.layer.val %*% W2.xR;
+    MSE.xR <- mean((Y.xR - Y.val)^2) #value of the function in point xR
+    
+    
+    if(MSE.xL < MSE.xR){
+      b <- xR
+      xR <- xL
+      xL <- b - k * (b - a)
+    }
+    else{
+      a <- xL
+      xL <- xR
+      xR <- a + k * (b - a)
+    }
+  }
+  C = (a + b) / 2;
+  
+  #use C to calculate final errors
+  if(n.hid <= n.elem){ #less neurons than samples
+    W2 <- ginv((diag(1, n.hid+1)/C) + t(hidden.layer)%*%hidden.layer) %*% t(hidden.layer) %*% Y.train
+  }
+  if (n.hid > n.elem) {
+    W2 <- t(hidden.layer) %*% ginv((diag(1, n.elem)/C) + hidden.layer%*%t(hidden.layer)) %*% Y.train
+  }
+  
+  Y.train.est = hidden.layer %*% W2
+  MSE.train <- mean((Y.train.est-Y.train)^2) #training error
+  
+  Y.val.est = hidden.layer.val %*% W2
+  MSE.val <- mean((Y.val.est-Y.val)^2) #validation error
+  
+  #do the prediction!
+  X.test <- as.matrix(cbind(fv.test, 1))
+
+  hidden.layer.test <- cbind(tanh(X.test%*%t(W1)), 1)
+  Y.test.est = hidden.layer.test %*% W2
+  MSE.test <- mean((Y.test.est - Y.test)^2)
+  
+  minmax.validation <- data.frame(min=0.85*Y.test, max=1.15*Y.test)
+  hit.rate.validation <- sapply(1:length(estimated.results.validation), function(x) {
+    return(ifelse(estimated.results.validation[x]>=minmax.validation[x, 'min'] && estimated.results.validation[x]<=minmax.validation[x, 'max'], 1, 0))
+  })
+  hit.rate.validation <- mean(hit.rate.validation)
+  
+  ret <- list()
+  ret[['observation.data.validation']] <- Y.test
+  ret[['estimated.results.validation']] <- Y.test.est
+  ret[['MSE.test']] <- MSE.test
+  ret[['hit.rate.validation']] <- hit.rate.validation
+  return(ret)
+}
+
 load("/home/felipe/2s2013/mo444/mo444/analysis/data.Rdata")
 # Build feature.vectors
 rand.data <<- data[sample(41514),]
@@ -307,3 +440,4 @@ regression.results <- mo444LinearRegression(use.grouped=F, min.retweets.count=1,
 decision.tree.results <- mo444DecisionTree(use.grouped=F, min.retweets.count=1, time.stamps=27)
 euclidian.distance.results <- mo444EuclidianDistanceToKMeans()
 svm.results <- mo444SVM(use.grouped=T)
+elm.results <- mo444NeuralNetworkELM(use.grouped=T)
